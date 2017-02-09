2025c4217c98f8415b928b1726af3931dadfbbb9
towobar <plux@freenet.de>
2017-02-09 16:04:45 +0100

    Init rRestaurant

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $users = DB::table('users')->get();
+
+        return view('home', ['users' => $users]);
+    }
+}
