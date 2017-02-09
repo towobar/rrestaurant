d4b709a1436011754432e598c65c4769b3d37359
towobar <plux@freenet.de>
2017-02-09 17:58:30 +0100

    change Panel Title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Restaurant
                 </div>
 
                 <div class="container">
