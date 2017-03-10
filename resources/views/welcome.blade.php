743db1f4d0521438badd038c9b70f1fc1e116348
towobar <plux@freenet.de>
2017-03-10 11:38:00 +0100

    change welcomeText

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Responsive Restaurant
+                    Responsive Restaurant !!
                 </div>
 
                 <div class="links">
